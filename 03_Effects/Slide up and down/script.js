3551ebd9f61e637d5e0773f2b311dfd35a3b2969
Yaseenjabir <yaseenkhan1235476@gmail.com>
2025-05-23 11:55:11 +0500

    Slide up and down animation

---
+++ 03_Effects/Slide up and down/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  $(".slideDown").click(function () {
+    $("div").slideDown(2000);
+  });
+  $(".slideUp").click(function () {
+    $("div").slideUp(2000);
+  });
+});
